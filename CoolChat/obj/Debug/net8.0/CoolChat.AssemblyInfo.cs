4a017880e5a17b24ae51f23f95fb21a6d17304d9
yakub1989 <yakub1989@interia.pl>
2024-06-11 20:39:21 +0200

    stylistic updates

--- CoolChat/obj/Debug/net8.0/CoolChat.AssemblyInfo.cs
+++ CoolChat/obj/Debug/net8.0/CoolChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoolChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afba8f1f6e2fae8dc9e786316a89cc71f707f053")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoolChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoolChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
